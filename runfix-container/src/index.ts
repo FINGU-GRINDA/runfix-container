84a17906fb7fcca12dc81d6accea5c4e9be4d7f6
vikyw <vikyw89@gmail.com>
2025-04-09 16:27:17 +0800

    feat: grinda translate

--- runfix-container/src/index.ts
+++ runfix-container/src/index.ts
@@ -1,2 +1,3 @@
 export { translateAndFit } from "./services/translate-and-fit.ts";
 export { translateAndFitToOriginalSize } from "./services/translate-and-fit-to-original.ts";
+export { getGrindaTranslateFn } from "./utils/grinda-translate.ts";
