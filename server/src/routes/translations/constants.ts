e4137d580ed46f62a55aaa67f2bfdb0514664ba0
vikyw <vikyw89@gmail.com>
2025-04-10 23:43:10 +0800

    feat: add more languages

--- server/src/routes/translations/constants.ts
+++ server/src/routes/translations/constants.ts
@@ -1,30 +1,192 @@
 /**
  * All supported language codes that match the database schema
- *
- * enText String? // English (USA)
- * koText String? // Korean
- * jaText String? // Japanese
- * zhText String? // Chinese
- * uzText String? // Uzbek (Uzbekistan)
- * viText String? // Vietnamese
- * ruText String? // Russian
- * kkText String? // Kazakh (Kazakhstan)
- * mnText String? // Mongolian (Mongolia)
- * thText String? // Thai
- * idText String? // Indonesian
+ * Complete list of ISO 639-1 language codes
  */
 export const allLanguageCodes = [
-  "en", // English (USA)
-  "ko", // Korean
-  "ja", // Japanese
-  "zh", // Chinese
-  "uz", // Uzbek (Uzbekistan)
-  "vi", // Vietnamese
-  "ru", // Russian
-  "kk", // Kazakh (Kazakhstan)
-  "mn", // Mongolian (Mongolia)
-  "th", // Thai
+  "aa", // Afar
+  "ab", // Abkhazian
+  "ae", // Avestan
+  "af", // Afrikaans
+  "ak", // Akan
+  "am", // Amharic
+  "an", // Aragonese
+  "ar", // Arabic
+  "as", // Assamese
+  "av", // Avaric
+  "ay", // Aymara
+  "az", // Azerbaijani
+  "ba", // Bashkir
+  "be", // Belarusian
+  "bg", // Bulgarian
+  "bh", // Bihari languages
+  "bi", // Bislama
+  "bm", // Bambara
+  "bn", // Bengali
+  "bo", // Tibetan
+  "br", // Breton
+  "bs", // Bosnian
+  "ca", // Catalan
+  "ce", // Chechen
+  "ch", // Chamorro
+  "co", // Corsican
+  "cr", // Cree
+  "cs", // Czech
+  "cu", // Church Slavic
+  "cv", // Chuvash
+  "cy", // Welsh
+  "da", // Danish
+  "de", // German
+  "dv", // Divehi
+  "dz", // Dzongkha
+  "ee", // Ewe
+  "el", // Greek
+  "en", // English
+  "eo", // Esperanto
+  "es", // Spanish
+  "et", // Estonian
+  "eu", // Basque
+  "fa", // Persian
+  "ff", // Fulah
+  "fi", // Finnish
+  "fj", // Fijian
+  "fo", // Faroese
+  "fr", // French
+  "fy", // Western Frisian
+  "ga", // Irish
+  "gd", // Gaelic
+  "gl", // Galician
+  "gn", // Guarani
+  "gu", // Gujarati
+  "gv", // Manx
+  "ha", // Hausa
+  "he", // Hebrew
+  "hi", // Hindi
+  "ho", // Hiri Motu
+  "hr", // Croatian
+  "ht", // Haitian
+  "hu", // Hungarian
+  "hy", // Armenian
+  "hz", // Herero
+  "ia", // Interlingua
   "id", // Indonesian
+  "ie", // Interlingue
+  "ig", // Igbo
+  "ii", // Sichuan Yi
+  "ik", // Inupiaq
+  "io", // Ido
+  "is", // Icelandic
+  "it", // Italian
+  "iu", // Inuktitut
+  "ja", // Japanese
+  "jv", // Javanese
+  "ka", // Georgian
+  "kg", // Kongo
+  "ki", // Kikuyu
+  "kj", // Kuanyama
+  "kk", // Kazakh
+  "kl", // Kalaallisut
+  "km", // Central Khmer
+  "kn", // Kannada
+  "ko", // Korean
+  "kr", // Kanuri
+  "ks", // Kashmiri
+  "ku", // Kurdish
+  "kv", // Komi
+  "kw", // Cornish
+  "ky", // Kirghiz
+  "la", // Latin
+  "lb", // Luxembourgish
+  "lg", // Ganda
+  "li", // Limburgan
+  "ln", // Lingala
+  "lo", // Lao
+  "lt", // Lithuanian
+  "lu", // Luba-Katanga
+  "lv", // Latvian
+  "mg", // Malagasy
+  "mh", // Marshallese
+  "mi", // Maori
+  "mk", // Macedonian
+  "ml", // Malayalam
+  "mn", // Mongolian
+  "mr", // Marathi
+  "ms", // Malay
+  "mt", // Maltese
+  "my", // Burmese
+  "na", // Nauru
+  "nb", // Norwegian Bokmål
+  "nd", // North Ndebele
+  "ne", // Nepali
+  "ng", // Ndonga
+  "nl", // Dutch
+  "nn", // Norwegian Nynorsk
+  "no", // Norwegian
+  "nr", // South Ndebele
+  "nv", // Navajo
+  "ny", // Nyanja
+  "oc", // Occitan
+  "oj", // Ojibwa
+  "om", // Oromo
+  "or", // Oriya
+  "os", // Ossetian
+  "pa", // Panjabi
+  "pi", // Pali
+  "pl", // Polish
+  "ps", // Pushto
+  "pt", // Portuguese
+  "qu", // Quechua
+  "rm", // Romansh
+  "rn", // Rundi
+  "ro", // Romanian
+  "ru", // Russian
+  "rw", // Kinyarwanda
+  "sa", // Sanskrit
+  "sc", // Sardinian
+  "sd", // Sindhi
+  "se", // Northern Sami
+  "sg", // Sango
+  "si", // Sinhala
+  "sk", // Slovak
+  "sl", // Slovenian
+  "sm", // Samoan
+  "sn", // Shona
+  "so", // Somali
+  "sq", // Albanian
+  "sr", // Serbian
+  "ss", // Swati
+  "st", // Southern Sotho
+  "su", // Sundanese
+  "sv", // Swedish
+  "sw", // Swahili
+  "ta", // Tamil
+  "te", // Telugu
+  "tg", // Tajik
+  "th", // Thai
+  "ti", // Tigrinya
+  "tk", // Turkmen
+  "tl", // Tagalog
+  "tn", // Tswana
+  "to", // Tonga
+  "tr", // Turkish
+  "ts", // Tsonga
+  "tt", // Tatar
+  "tw", // Twi
+  "ty", // Tahitian
+  "ug", // Uighur
+  "uk", // Ukrainian
+  "ur", // Urdu
+  "uz", // Uzbek
+  "ve", // Venda
+  "vi", // Vietnamese
+  "vo", // Volapük
+  "wa", // Walloon
+  "wo", // Wolof
+  "xh", // Xhosa
+  "yi", // Yiddish
+  "yo", // Yoruba
+  "za", // Zhuang
+  "zh", // Chinese
+  "zu", // Zulu
 ];
 
 /**
@@ -34,12 +196,12 @@ export const allLanguageCodes = [
  */
 export const languageToDbCode = (params: { languageCode: string }): string => {
   const { languageCode } = params;
-  
+
   // Validate that the language code is supported
   if (!allLanguageCodes.includes(languageCode)) {
     throw new Error(`Unsupported language code: ${languageCode}`);
   }
-  
+
   return `${languageCode}Text`;
 };
 
@@ -50,19 +212,19 @@ export const languageToDbCode = (params: { languageCode: string }): string => {
  */
 export const dbCodeToLanguage = (params: { dbCode: string }): string => {
   const { dbCode } = params;
-  
+
   // Check if the dbCode ends with "Text"
   if (!dbCode.endsWith("Text")) {
     throw new Error(`Invalid database field name: ${dbCode}`);
   }
-  
+
   // Extract the language code
   const languageCode = dbCode.replace(/Text$/, "");
-  
+
   // Validate that the language code is supported
   if (!allLanguageCodes.includes(languageCode)) {
     throw new Error(`Unsupported language code: ${languageCode}`);
   }
-  
+
   return languageCode;
 };
