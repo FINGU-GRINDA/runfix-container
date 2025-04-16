1049b9251b52de4d0aaad299a063dc0cc4bcef58
vikyw <vikyw89@gmail.com>
2025-04-16 21:19:33 +0800

    fix: build error

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -6,5 +6,6 @@ const withNextra = nextra({
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
+	output: "standalone",
 	// ... Other Next.js config options
 });
