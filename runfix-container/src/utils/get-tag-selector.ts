7d88bd8f6ece9c22f8562dfabf388da1b3119d20
vikyw <vikyw89@gmail.com>
2025-04-28 21:58:38 +0800

    fix: update runfix-container version to 0.9.1 in package files and improve getTagSelector function, add cache

--- runfix-container/src/utils/get-tag-selector.ts
+++ runfix-container/src/utils/get-tag-selector.ts
@@ -1,21 +1,19 @@
 export function getTagSelector(params: { element: HTMLElement }): string {
+  // cache it
+  if (params.element.hasAttribute("data-tag-selector")) {
+    return params.element.getAttribute("data-tag-selector") || "";
+  }
   const path: string[] = [];
   let current: HTMLElement | null = params.element;
 
   while (current) {
     const tagName = current.tagName.toLowerCase();
-    const siblings = Array.from(current.parentElement?.children || []);
-    const sameTagSiblings = siblings.filter((sibling) => sibling.tagName === current?.tagName);
-
-    if (sameTagSiblings.length > 1) {
-      const index = sameTagSiblings.indexOf(current) + 1;
-      path.unshift(`${tagName}:nth-of-type(${index})`);
-    } else {
-      path.unshift(tagName);
-    }
-
+    path.unshift(tagName);
     current = current.parentElement;
   }
 
-  return path.join(" > ");
+  const tagSelector = path.join(" > ");
+
+  params.element.setAttribute("data-tag-selector", tagSelector);
+  return tagSelector;
 }
